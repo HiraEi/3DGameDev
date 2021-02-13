0c77625c3bb6c9a0cfe0be3d78f05d757e13a89b
HiraEi <aeblanket@gmail.com>
2021-02-13 14:32:03 +0900

    障害物を消去するスクリプトとオブジェクトを生成するスクリプトを追加

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    /// <summary>
+    /// 生成したいオブジェクト
+    /// </summary>
+    [SerializeField] GameObject obstaclePrefab = null;
+
+    /// <summary>
+    /// 生成する時間間隔
+    /// </summary>
+    [SerializeField] float generatTime = 0f;
+
+    IEnumerable generat()
+    {
+        Instantiate(obstaclePrefab, this.transform);
+
+        yield return new WaitForSeconds(generatTime);
+    }
+}

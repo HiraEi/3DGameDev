68db192e0c4041d6586c906627fbde052023b8fb
HiraEi <aeblanket@gmail.com>
2021-02-16 00:54:36 +0900

    扉のクリア条件を満たした時、クリアフラグを有効にし、クリア演出が再生されるようにした

--- Assets/Scripts/PanelScripts/PanelManager.cs
+++ Assets/Scripts/PanelScripts/PanelManager.cs
@@ -5,31 +5,31 @@ using UnityEngine.Playables;
 
 public class PanelManager : MonoBehaviour
 {
+    /// <summary> クリア枚数の設定
     [SerializeField] int clearCount = 0;
+    /// <summary>プレイヤーが踏んだパネルの枚数
     public int playCount = 0;
-
-    bool firstFlag = false;
+    /// <summary> 最初の扉のクリア枚数設定
     int firstClearCount = 4;
-
+    /// <summary> 最初の扉のクリアフラグ
+    bool firstFlag = false;
+    /// <summary> 最後の扉のクリアフラグ
     bool lastFlag = false;
-
-    [SerializeField]PlayableDirector[] director;
+    /// <summary>　扉のクリア演出の設定
+    [SerializeField] PlayableDirector[] director = null;
 
     void Update()
     {
-        //if (clearCount == playCount)
-        //{
-        //    Debug.Log("クリア");
-        //}
-
-        if(playCount == firstClearCount)
+        if(playCount == firstClearCount && !firstFlag)
         {
             firstFlag = true;
+            director[0].Play();
         }
 
-        if(playCount == clearCount)
+        if(playCount == clearCount && !lastFlag)
         {
             lastFlag = true;
+            director[1].Play();
         }
     }
 }

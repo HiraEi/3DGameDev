e310eb0be2399ca96df0cf892586ab6f9bc382e8
HiraEi <aeblanket@gmail.com>
2021-02-24 17:39:26 +0900

    看板を装飾した

---
+++ Assets/Scripts/BillboardController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardController : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.forward = Camera.main.transform.forward;
+    }
+}
--- Assets/Scripts/GManager.cs
+++ Assets/Scripts/GManager.cs
@@ -22,6 +22,6 @@ public class GManager : MonoBehaviour
 
     //void Update()
     //{
-        
+
     //}
 }

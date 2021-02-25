4913e22139a3b823914ad04036ee8e6e4711c47f
HiraEi <aeblanket@gmail.com>
2021-02-25 18:53:41 +0900

    UIを追加した。ライトギミックの正解のクリスタルに触れた時のSEを追加した

--- Assets/Scripts/LightScripts/LightController.cs
+++ Assets/Scripts/LightScripts/LightController.cs
@@ -6,15 +6,19 @@ public class LightController : MonoBehaviour
 {
     GameObject m_lightManager;
     LightManager m_manager;
+    AudioSource m_audio;
+    [SerializeField] AudioClip clearSE = null;
     
     void Start()
     {
         m_lightManager = GameObject.Find("LightManager");
         m_manager = m_lightManager.GetComponent<LightManager>();
+        m_audio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         m_manager.m_gimmikClear = true;
+        m_audio.PlayOneShot(clearSE);
     }
 }

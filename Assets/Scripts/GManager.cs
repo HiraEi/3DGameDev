f7a9b0ea48341bc95c9df99adac3d8a35bc6398f
HiraEi <aeblanket@gmail.com>
2021-02-27 02:39:14 +0900

    シーンの遷移時のSEを追加した

--- Assets/Scripts/GManager.cs
+++ Assets/Scripts/GManager.cs
@@ -5,23 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class GManager : MonoBehaviour
 {
+    AudioSource m_audio;
+    [SerializeField] AudioClip clip = null;
+
+    private void Start()
+    {
+        m_audio = GetComponent<AudioSource>();
+    }
+
     public void LightLoad()
     {
+        m_audio.PlayOneShot(clip);
         SceneManager.LoadScene(1);
     }
 
     public void PanelLoad()
     {
+        m_audio.PlayOneShot(clip);
         SceneManager.LoadScene(2);
     }
 
     public void EndLoad()
     {
+        m_audio.PlayOneShot(clip);
         SceneManager.LoadScene(3);
     }
 
     public void TitleLoad()
     {
+        m_audio.PlayOneShot(clip);
         SceneManager.LoadScene(0);
     }
 }

98a2f1bf136c233a04db20a7af979a1068c91d5c
HiraEi <aeblanket@gmail.com>
2021-02-27 02:22:03 +0900

    ライトシーン、パネルシーン、エンディングシーンにBGMを追加した

--- Assets/Scripts/EndingPlayerAnim.cs
+++ Assets/Scripts/EndingPlayerAnim.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class EndingPlayerAnim : MonoBehaviour
 {
     Animator m_anim;
-    bool play = false;
 
     void Start()
     {
         m_anim = GetComponent<Animator>();
         m_anim.SetBool("play",true);
-        Debug.Log(play);
     }
 }

c3b12934b08fddc96eb03f8b9c85e770ac2fe750
HiraEi <aeblanket@gmail.com>
2021-02-27 01:47:46 +0900

    エンディングシーンを完成させた

---
+++ Assets/Scripts/EndingPlayerAnim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingPlayerAnim : MonoBehaviour
+{
+    Animator m_anim;
+    bool play = false;
+
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        m_anim.SetBool("play",true);
+        Debug.Log(play);
+    }
+}
